511c045a42509520b97ba1d754e1fd9982bfff72
ezequielsetzes <ezequiel.setzes@globant.com>
2022-04-11 12:19:00 -0300

    business card implementation

---
+++ lib/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'utils/constants.dart';
+import 'utils/text_styles.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(Constants.padding),
+              child: CircleAvatar(
+                backgroundImage: AssetImage(
+                  Constants.avatarPath,
+                ),
+                radius: Constants.radiusAvatar,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(Constants.padding),
+              child: Text(
+                Constants.name,
+                style: TextStyles.nameTextStyle,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(Constants.padding),
+              child: Text(
+                Constants.job,
+                style: TextStyles.jobTextStyle,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(Constants.padding),
+              child: Text(
+                Constants.mail,
+                style: TextStyles.mailTextStyle,
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const double padding = 25.0;
+  static const double nameFontSize = 30.0;
+  static const double jobFontSize = 25.0;
+  static const double mailFontSize = 20.0;
+  static const double radiusAvatar = 100.0;
+  static const String avatarPath = 'assets/images/avatar.png';
+  static const String name = 'Ezequiel Hernan Setzes';
+  static const String job = 'Flutter Mobile Developer';
+  static const String mail = 'ezequielsetzes1904@gmail.com';
+}
